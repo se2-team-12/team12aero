e01bc00df5b1b44825398e1a120a402803123341
jzamora3 <39277028+jzamora3@users.noreply.github.com>
2018-11-03 16:56:35 -0500

    Add files via upload
    New updated routes with daily diagnostic capabilities

---
+++ routes/clientSide.js
@@ -0,0 +1,356 @@
+const express = require('express');
+const router = express.Router({});
+const mongoose = require('mongoose');
+mongoose.connect('localhost/swe2');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+var bcryptNodejs = require("bcrypt-nodejs");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
+
+let Heartbeat = require('../models/heartbeatModel');
+let Diagnostic = require('../models/diagnosticModel'); 
+let User = require('../models/userModel');
+let Gateway = require('../models/gatewayModel');
+let dailyDiagnostic = require('../models/dailyDiagnosticResultModel');  
+
+const validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+router.post('/register', function(req, res) {
+  var new_user = new User({
+    userName: req.body.userName,
+    password: generateHash(req.body.password),
+    email: req.body.email
+  });
+
+  new_user.save()
+          .then(result => {
+            console.log(result);
+            res.status(201).json({
+                User: result,
+                message: "POST Success" 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.post('/login', function(req, res) {
+  const userName = req.body.userName;
+  const password = req.body.password
+  User.findOne({userName: req.body.userName}, function(err, user) {
+
+
+    if (!bcrypt.compareSync(password, user.password)) {
+            res.json({
+             status: 0,
+             message: err
+         });
+
+    } else {
+            res.json({
+                status: 1,
+                message: "Successful login"
+            });
+    }
+  });
+});
+
+/* GET gateway listing. */
+router.get('/', function (req, res) {
+    Heartbeat.find()
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+});
+
+
+
+router.post('/users/login', function(req, res) {
+     let email = req.body.email;
+     let password = req.body.password;
+     let userName = req.body.userName;
+     let data;
+    if (email.length > 0 && password.length > 0) {
+         data = {
+             email: email,
+             password: password
+         };
+    }
+    else if(username.length > 0 && password.length > 0) {
+         data = {
+             username: username,
+             password: password
+         };
+    } 
+    else {
+         res.json({
+             status: 0,
+             message: err
+         });
+    }
+
+
+    User.findOne(data, function(err, user) {
+        if (err) {
+             res.json({
+                 status: 0,
+                 message: err
+             });
+        }
+        else if (!user) {
+             res.json({
+                 status: 0,
+                 msg: "not found"
+             });
+         }
+        else if (user){
+            res.json({
+                status: 1,
+                message: "Successful login"
+            });
+        }
+     
+        else {
+            res.json({
+                status: 0,
+                msg: "Invalid Fields"
+            });
+        }
+    });
+});
+
+router.get("/:GatewayId", (req, res, next) => {
+  const id = req.params.GatewayId;
+  let query = {GatewayId: id}
+  Heartbeat.find(query)
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.put('/', function (req, res) {
+    res.status(202).json({
+        "Status": "put ok"
+    });
+    //res.status(202).send();
+});
+
+router.get("/onDemand/:GatewayId", (req, res, next) => {
+    const id = req.params.GatewayId;
+    let query = {GatewayId: id}
+
+    Diagnostic.find(query)
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+});
+
+router.get("/dailyDiagnostic/:GatewayId", (req, res, next) => {
+    const id = req.params.GatewayId;
+    let query = {GatewayId: id}
+
+    dailyDiagnostic.find(query)
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+});
+
+
+router.put('/', function (req, res) {
+    res.status(202).json({
+        "Status": "put ok"
+    });
+    //res.status(202).send();
+});
+
+
+
+router.post('/', function (req, res) {
+
+    let ODDTest = new Diagnostic({
+        ODD: req.body.ODD,
+        GatewayId: req.body.GatewayId,
+  
+    });
+
+    ODDTest
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                Diagnostic: result,
+                message: "POST Success" 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.post('/dailyDiagnostic', function (req, res) {
+
+    let dailyDiagnosticTest = new dailyDiagnostic({
+        dailyHour: req.body.dailyHour,
+        dailyMin: req.body.dailyMin,
+        dailySecond: req.body.dailySecond,
+        GatewayId: req.body.GatewayId,
+        Type: req.body.Type,
+        DDD: req.body.DDD
+  
+    });
+
+    dailyDiagnosticTest
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                Diagnostic: result,
+                message: "Success" 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.post('/createGateway', function (req, res) {
+    let newGateway = new Gateway({
+        GatewayId: new mongoose.Types.ObjectId(),
+        userName: req.body.userName,
+        email: req.body.email,
+        token: req.body.email
+  
+    });
+
+    newGateway
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                Gateway: result,
+                message: "New Gateway created" 
+            });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+router.post('/user', function (req, res, bcrypt) {
+
+  const password = req.body.password
+ 
+
+    const salt = bcryptNodejs.genSaltSync(10);
+    const hash = bcryptNodejs.hashSync(password, salt);
+
+
+    let newUser = new User({
+        email: req.body.email,
+        userName: req.body.userName,
+        password: hash,
+  
+    });
+
+    newUser    
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                User: result,
+                message: "POST Success" 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+
+
+router.delete("/:GatewayId", (req, res, next) => {
+  const id = req.params.GatewayId;
+  Heartbeat.remove({ _id: id })
+    .exec()
+    .then(result => {
+        console.log("Heartbeat deleted");
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+
+router.options('/', function (req, res) {
+    res.header('Allow', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD').status(204).send();
+});
+
+module.exports = router;
---
+++ routes/gateway.js
@@ -0,0 +1,278 @@
+var express = require('express');
+var router = express.Router({});
+var mongoose = require('mongoose');
+mongoose.connect('localhost/swe2');
+var Schema = mongoose.Schema;
+
+var Heartbeat = require('../models/heartbeatModel');
+var Diagnostic = require('../models/diagnosticModel'); 
+let Gateway = require('../models/gatewayModel'); 
+let dailyDiagnostic = require('../models/dailyDiagnosticResultModel');
+
+
+
+/* GET gateway listing. */
+router.get("/:GatewayId", (req, res, next) => {
+    var token = req.headers['accesstoken'];
+    let tokenId = req.body.Token
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  
+    if (token == tokenId){
+    const id = req.params.GatewayId;
+    var query = {GatewayId: id}
+
+    Diagnostic.find(query)
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    }
+    else{
+        return res.status(401).send({ auth: false, message: 'Invalid Token.' });
+    }
+    
+
+});
+
+
+
+//Posting a heartbeat and recieving diagnostics
+router.post('/heartbeat/:GatewayId', function (req, res) {
+    const id = req.params.GatewayId;
+    var query = {GatewayId: id}
+    var token = req.headers['accesstoken'];
+    let tokenId = req.body.Token
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  
+    if (token == tokenId){
+    var test2 = ""
+    var test = "";
+    Diagnostic.find(query)
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            test = docs;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    dailyDiagnostic.find(query)
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            test2 = docs;
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+        var heartbeat = new Gateway({
+        GatewayId: req.body.GatewayId,
+        TimeStamp: req.body.TimeStamp,
+    });
+
+    heartbeat
+        .save()
+        .then(result => {
+            console.log(result);
+
+            res.status(201).json({
+                postedHeartbeat: result,
+                odd: test,
+                ddd: test2,
+                message: "Success" 
+            });
+    Diagnostic.remove({ GatewayId: id })
+        .exec()
+        .then(result => {
+            console.log("Diagnostics deleted");
+            res.status(200).json(result);
+    })
+    dailyDiagnostic.remove({ GatewayId: id })
+        .exec()
+        .then(result => {
+            console.log("Diagnostics deleted");
+            res.status(200).json(result);
+    })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    }
+    else{
+        return res.status(401).send({ auth: false, message: 'Invalid Token.' });
+    }
+   
+});
+//Post a diagnostic test
+router.post('/diagnostic/test', function (req, res) {
+
+    var ODDTest = new Diagnostic({
+        ODD: req.body.ODDType,
+        GatewayId: req.body.GatewayId,
+        Type: req.body.Type,
+        Result: req.body.Result,
+        IsClear: req.body.IsClear,
+  
+    });
+    var token = req.headers['accesstoken'];
+    let tokenId = req.body.Token
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  
+    if (token == tokenId){
+
+    ODDTest
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                Diagnostic: result,
+                message: "POST Success" 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    }
+    else{
+        return res.status(401).send({ auth: false, message: 'Invalid Token.' });
+    }
+});
+
+router.post('/dailyDiagnostic/test', function (req, res) {
+
+    var DDDTest = new dailyDiagnostic({
+
+        GatewayId: req.body.GatewayId,
+        Type: req.body.Type,
+        Result: req.body.Result,
+        Time: req.body.Time,
+  
+    });
+    var token = req.headers['accesstoken'];
+    let tokenId = req.body.Token
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  
+    if (token == tokenId){
+
+    DDDTest
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                Diagnostic: result,
+                message: "POST Success" 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    }
+    else{
+        return res.status(401).send({ auth: false, message: 'Invalid Token.' });
+    }
+});
+router.post('/newGateway', function (req, res) {
+
+    var newGateway = new Gateway({
+        GatewayId: req.body.GatewayId,
+
+    });
+    const id = newGateway.GatewayId;
+    var query = {GatewayId: id}
+
+
+    Gateway.find(query)
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+});
+
+router.put('/', function (req, res) {
+        var heartbeat = new Heartbeat({
+        GatewayId: req.body.GatewayId,
+        TimeStamp: req.body.TimeStamp,
+        status: req.body.status
+    });
+  gateway
+  .findOneAndUpdate({_id: req.params.GatewayId}, req.body, {new: true}, function(err, heartbeat) {
+    if (err)
+      res.send(err);
+    res.json(heartbeat);
+  });
+
+});
+
+router.delete('/', function (req, res) {
+        var heartbeat = new Heartbeat({
+        GatewayId: req.body.GatewayId,
+        TimeStamp: req.body.TimeStamp,
+        status: req.body.status
+    });
+  gateway.remove({
+    _id: req.params.GatewayId
+  }, function(err, heartbeat) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Heartbeat successfully deleted' });
+  });
+    //res.status(202).send();
+    process.exit();
+});
+
+router.patch("/:GatewayId", (req, res, next) => {
+  const id = req.params.GatewayId;
+  const updateOps = {};
+  for (const ops of Object.keys(updateOps)) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Diagnostic.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.options('/', function (req, res) {
+    res.header('Allow', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD').status(204).send();
+});
+
+module.exports = router;
