1e838fcc1ba99eba033cbe63b40ab66bbcc05f7b
jzamora3 <39277028+jzamora3@users.noreply.github.com>
2018-11-10 18:01:49 -0600

    Update users.js
    Trying to add new users rout again

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,115 @@
-var express = require('express');
-var router = express.Router();
- /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+module.exports = router;
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+var bcryptNodejs = require("bcrypt-nodejs");
+
+const Users = require("../models/user");
+
+router.post("/signup", (req, res, next) => {
+  Users.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail exists"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+          } else {
+            const user = new Users({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            });
+            user
+              .save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created"
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        });
+      }
+    });
 });
- module.exports = router;
+
+router.post("/login", (req, res, next) => {
+  Users.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            "secret",
+            {
+                expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+            email: req.body.email,
+            status: 1,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+          status: 0,
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "User deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
