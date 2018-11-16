fb12171292ffe862dda02146794a1aa5b1917d89
jzamora3 <39277028+jzamora3@users.noreply.github.com>
2018-11-15 19:43:56 -0600

    Update app.js
    change db name to team12aero

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let clientSideRouter = require('./routes/clientSide');
 let newUsersRouter = require('./routes/tokenUser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/swe2', { useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect('mongodb://localhost:27017/team12aero', { useNewUrlParser: true, useCreateIndex: true});
 
 
 const app = express();
